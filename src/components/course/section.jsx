3a9e6688c442cda5ea3b051ada500af9537a8d72
Gokul0404 <gokul332020@gmail.com>
2023-04-20 13:58:19 +0530

    dfgd

--- src/components/course/section.jsx
+++ src/components/course/section.jsx
@@ -1,6 +1,10 @@
-import React from "react";
-import '../course/Section.css'
+import React, { useState } from "react";
+import "../course/Section.css";
+import drop from '../course/images/drop.png'
+import { coursedata } from "../course/SectionData";
 export default function Course() {
+  const [dragStatus, setdragStatus] = useState(false);
+  const [dragData, setDragData] = useState(false);
   return (
     <div>
       <div className="bg-black w-screen  pt-[13vh] ">
@@ -10,46 +14,88 @@ export default function Course() {
         >
           <div className="flex justify-start pl-[10%] gap-x-[3%] ">
             <div className=" w-[70vw] h-[80vh] outline outline-white rounded-[20px] overflow-hidden ">
-              <div className=" bg-white opacity-40 w-[100%] h-[100%] ">d</div>
+              {/* screen */}
+              {console.log(dragData)}
+              <div
+                className=" bg-white  w-[100%] h-[100%] "
+                onDragEnter={(e) => setdragStatus(true)}
+              >
+                {dragStatus ? (
+                  <video autoPlay muted loop className=" object-cover">
+                    <source src={dragData} />
+                  </video>
+                ) : (
+                  <img
+                    className="object-cover w-full h-full border-[8px] border-black "
+                    src={drop}
+                  />
+                )}
+              </div>
             </div>
+
+            {/* tool */}
             <div className="w-[10vw] h-[80vh] relative flex justify-center items-center">
               <div className="w-[100%]  flex justify-center flex-col items-center gap-5 text-center">
                 <div
-                  className="w-[5vw] h-[10vh]  bg-gray-600 rounded-md"
+                  className="w-[70px] h-[65px]  bg-gray-600 rounded-md"
                   draggable="true"
+                  onDragStart={() => {
+                    setdragStatus(false);
+                    setDragData("./v1.mp4");
+
+                    // "https://media.tenor.com/evhtGNYJy3sAAAAC/bucksin6-austinpowersjohnson.gif"
+                  }}
                 >
-                  a
+                  first
                 </div>
                 <div
-                  className="w-[5vw] h-[10vh] bg-gray-600 rounded-md"
+                  className="w-[70px] h-[65px] bg-gray-600 rounded-md"
                   draggable="true"
+                  onDragStart={() => {
+                    setdragStatus(false);
+                    setDragData(
+                      "./v2.mp4"
+                    );
+                  }}
                 >
-                  a
+                  second
                 </div>
                 <div
-                  className="w-[5vw] h-[10vh] bg-gray-600  rounded-md"
+                  className="w-[70px] h-[65px] bg-gray-600  rounded-md"
                   draggable="true"
+                  onDragStart={() => {
+                    setdragStatus(false);
+                    setDragData("./v3.mp4");
+                  }}
                 >
-                  a
+                  third
                 </div>
                 <div
-                  className="w-[5vw] h-[10vh] bg-gray-600 rounded-md"
+                  className="w-[70px] h-[65px] bg-gray-600 rounded-md"
                   draggable="true"
+                  onDragStart={() => {
+                    setdragStatus(false);
+                    setDragData("./v4.mp4");
+                  }}
                 >
-                  a
+                  fourth
                 </div>
                 <div
-                  className="w-[5vw] h-[10vh]  bg-gray-600 rounded-md"
+                  className="w-[70px] h-[65px]  bg-gray-600 rounded-md"
                   draggable="true"
+                  onDragStart={() => {
+                    setdragStatus(false);
+                    setDragData("./v5.mp4");
+                  }}
                 >
-                  a
+                  fifth
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div>
-          <div className="flex flex-col justify-center items-center text-white py-10">
+          <div className="flex flex-col justify-center items-center text-white py-[10vh]">
             <h2 className="text-fblue   fontbold">COURSE</h2>
             <p className="text-center w-[80vw]">
               You can use any value defined in your opacity scale, or use
@@ -57,43 +103,55 @@ export default function Course() {
               arbitrary values if you need to deviate from your design tokens.
             </p>
           </div>
-          <div className="flex justify-center">
-            <div
-              className="w-[80vw] py-5 laptop:flex flex-row flex-wrap
-             items-center gap-10 justify-center "
-            >
-              <div className="flex justify-center py-5 mobile-sm:basis-4/5 tablet:basis-2 laptop:basis-1">
-                <div className="bg-white mobile-sm:w-[80vw] mobile-sm:h-[70vh] mobile-md:w-[60vw] mobile-md:h-[75vh] tablet:w-[40vw] laptop:w-[20vw] laptop:h-[65vh] rounded-2xl flex items-center flex-col">
-                  <div className="bg-gray-400 w-[90%] mobile-sm:h-[30vh] laptop:h-[20vh] mt-4 rounded-2xl"></div>
-                  <div></div>
-                </div>
-              </div>
 
-              <div className="flex justify-center py-5 mobile-sm:basis-4/5 tablet:basis-2 laptop:basis-1">
-                <div className="bg-white mobile-sm:w-[80vw] mobile-sm:h-[70vh] mobile-md:w-[60vw] mobile-md:h-[75vh] tablet:w-[40vw] laptop:w-[20vw] laptop:h-[65vh] rounded-2xl flex items-center flex-col">
-                  <div className="bg-gray-400 w-[90%] mobile-sm:h-[30vh] laptop:h-[20vh] mt-4 rounded-2xl"></div>
-                  <div></div>
-                </div>
-              </div>
-              <div className="flex justify-center py-5 mobile-sm:basis-4/5 tablet:basis-2 laptop:basis-1">
-                <div className="bg-white mobile-sm:w-[80vw] mobile-sm:h-[70vh] mobile-md:w-[60vw] mobile-md:h-[75vh] tablet:w-[40vw] laptop:w-[20vw] laptop:h-[65vh] rounded-2xl flex items-center flex-col">
-                  <div className="bg-gray-400 w-[90%] mobile-sm:h-[30vh] laptop:h-[20vh] mt-4 rounded-2xl"></div>
-                  <div></div>
-                </div>
-              </div>
-              <div className="flex justify-center py-5 mobile-sm:basis-4/5 tablet:basis-2 laptop:basis-1">
-                <div className="bg-white mobile-sm:w-[80vw] mobile-sm:h-[70vh] mobile-md:w-[60vw] mobile-md:h-[75vh] tablet:w-[40vw] laptop:w-[20vw] laptop:h-[65vh] rounded-2xl flex items-center flex-col">
-                  <div className="bg-gray-400 w-[90%] mobile-sm:h-[30vh] laptop:h-[20vh] mt-4 rounded-2xl"></div>
-                  <div></div>
-                </div>
-              </div>
-              <div className="flex justify-center py-5 mobile-sm:basis-4/5 tablet:basis-2 laptop:basis-1">
-                <div className="bg-white mobile-sm:w-[80vw] mobile-sm:h-[70vh] mobile-md:w-[60vw] mobile-md:h-[75vh] tablet:w-[40vw] laptop:w-[20vw] laptop:h-[65vh] rounded-2xl flex items-center flex-col">
-                  <div className="bg-gray-400 w-[90%] mobile-sm:h-[30vh] laptop:h-[20vh] mt-4 rounded-2xl"></div>
-                  <div></div>
-                </div>
-              </div>
-            </div>
+          <div className={`flex flex-row flex-wrap w-screen justify-center  `}>
+            {coursedata.map((res) => {
+              return (
+                <>
+                  <div className="laptop:basis-1/4 py-10 px-10">
+                    <div
+                      className={`bg-white  mobile-sm:w-[80vw] mobile-sm:h-[70vh] mobile-md:w-[60vw] mobile-md:h-[75vh] tablet:w-[40vw] laptop:w-[20vw] laptop:h-[65vh] rounded-2xl flex items-center flex-col`}
+                    >
+                      <div className="bg-gray-400 w-[90%] mobile-sm:h-[30vh]  laptop:h-[20vh] mt-4 rounded-2xl"></div>
+                      <div className="text-mes self-start pl-5 pt-3 gap-y-2 flex flex-col">
+                        <div className="flex">
+                          <p className="minih">COURSE TITLE:</p>
+                          <p>{res.title}</p>
+                        </div>
+                        <div className="flex">
+                          <p className="minih">COURSE DESCRIPTION:</p>
+                          <p>{res.description}</p>
+                        </div>
+                        <div className="flex">
+                          <p className="minih">DURATION:</p>
+                          <p>{res.duration}</p>
+                        </div>
+                        <div className="flex">
+                          <p className="minih">PREREQUISITES:</p>
+                          <p>{res.prerequisites}</p>
+                        </div>
+                        <div className="flex">
+                          <p className="minih">COURSE FORMATE:</p>
+                          <p>{res.formate}</p>
+                        </div>
+                        <div className="flex">
+                          <p className="minih">COURSE MATERIALS:</p>
+                          <p>{res.materials}</p>
+                        </div>
+                        <div className="flex">
+                          <p className="minih">ASSESMENT AND GRADING:</p>
+                          <p>{res.assesment}</p>
+                        </div>
+                        <div className="flex">
+                          <p className="minih">COURSE FEE:</p>
+                          <p>{res.fee}</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
           </div>
         </div>
       </div>
