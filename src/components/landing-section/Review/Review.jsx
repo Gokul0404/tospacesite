b64f5ddafcbdfa67cec0c16463fb0497038a670e
Gokul0404 <gokul332020@gmail.com>
2023-04-21 17:31:03 +0530

    232

--- src/components/landing-section/Review/Review.jsx
+++ src/components/landing-section/Review/Review.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../Review/Review.css";
+import {reviewdata} from '../Review/Reviewdata'
 import {motion} from 'framer-motion'
 import { duration } from "@mui/material";
 export default function Review() {
@@ -11,7 +12,9 @@ export default function Review() {
             <h2 className="fontbold text-fblue">TESTIMONIALS</h2>
             <h2 className="text-soon ">WHAT OUR CUSTOMERS SAY</h2>
           </div>
-          <div>
+
+        
+          <div className="parafont">
             <p>
               Lorem ipsum is simply ghtryhtn rtb hdummy text of the printing and
               typesetting industry. Lorem ipsum has been the industry's standard
@@ -21,128 +24,40 @@ export default function Review() {
           </div>
         </div>
       </div>
-      <div className=" flex justify-center py-10">
-        <div className="w-[90%]  mobile-md:flex-col laptop:flex-row flex items-center justify-center ">
-          <div className="w-[30%]">
-            <div className="h-[30vh] flex  z-50 justify-center pl-2 ">
-              <motion.div
-                whileHover={{ scale: 1.1, transition: { duration: 0.5 } }}
-                exit={{ scale: 0, transition: { duration: 2 } }}
-                className="w-[100%] "
-              >
-                <div className="w-[90%] h-[25vh]  box flex justify-center items-center text-mes   ">
-                  <p className="w-[90%] text-justify z-20">
-                    printing and typesetting industry. Lorem ipsum some that
-                    industr's standard dummy text ever since the and them all
-                    that 1500s.
-                  </p>
-                </div>
-                <div className="triangle-down z-10"></div>
-              </motion.div>
-            </div>
-            <div className="">
-              <div className="   flex items-center  ">
-                <div className="w-[70px] h-[70px] rounded-full bg-white overflow-hidden img-roud roundimg ">
-                  <img src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg" />
-                </div>
-                <div className="pl-5">
-                  <p>Raju</p>
-                  <p className="pt-1">Subject who they are</p>
-                </div>
-              </div>
-            </div>
-          </div>
 
-          <div className="w-[30%]">
-            <div className="h-[30vh] flex  z-50 justify-center pl-2 ">
-              <motion.div
-                whileHover={{ scale: 1.1, transition: { duration: 0.5 } }}
-                exit={{ scale: 0, transition: { duration: 2 } }}
-                className="w-[100%] "
-              >
-                <div className="w-[90%] h-[25vh]  box flex justify-center items-center text-mes   ">
-                  <p className="w-[90%] text-justify z-20">
-                    printing and typesetting industry. Lorem ipsum some that
-                    industr's standard dummy text ever since the and them all
-                    that 1500s.
-                  </p>
-                </div>
-                <div className="triangle-down z-10"></div>
-              </motion.div>
-            </div>
-            <div className="">
-              <div className="   flex items-center  ">
-                <div className="w-[70px] h-[70px] rounded-full bg-white overflow-hidden img-roud roundimg ">
-                  <img src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg" />
-                </div>
-                <div className="pl-5">
-                  <p>Raju</p>
-                  <p className="pt-1">Subject who they are</p>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="w-[30%]">
-            <div className="h-[30vh] flex  z-50 justify-center pl-2 ">
-              <motion.div
-                whileHover={{ scale: 1.1, transition: { duration: 0.5 } }}
-                exit={{ scale: 0, transition: { duration: 2 } }}
-                className="w-[100%] "
-              >
-                <div className="w-[90%] h-[25vh]  box flex justify-center items-center text-mes   ">
-                  <p className="w-[90%] text-justify z-20">
-                    printing and typesetting industry. Lorem ipsum some that
-                    industr's standard dummy text ever since the and them all
-                    that 1500s.
-                  </p>
-                </div>
-                <div className="triangle-down z-10"></div>
-              </motion.div>
-            </div>
-            <div className="">
-              <div className="   flex items-center  ">
-                <div className="w-[70px] h-[70px] rounded-full bg-white overflow-hidden img-roud roundimg ">
-                  <img src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg" />
-                </div>
-                <div className="pl-5">
-                  <p>Raju</p>
-                  <p className="pt-1">Subject who they are</p>
+      <div className="mobile-sm:flex mobile-sm:flex-col mobile-sm:justify-center laptop:flex  laptop:flex-row justify-center px-5 py-10">
+          {reviewdata.map((data) => {
+            return (
+              <div className="w-[80%]   mobile-md:flex-col laptop:flex-row flex items-center justify-center ">
+                <div className="w-[90%]">
+                  <div className="py-5 flex  z-50 justify-center pl-2 ">
+                    <motion.div
+                      whileHover={{ scale: 1.1, transition: { duration: 0.5 } }}
+                      exit={{ scale: 0, transition: { duration: 2 } }}
+                      className="w-[100%] "
+                    >
+                      <div className="w-[90%] py-5  box flex justify-center items-center text-mes   ">
+                        <p className="px-5 text-justify z-20 ">{data.mes}</p>
+                      </div>
+                      <div className="triangle-down z-10"></div>
+                    </motion.div>
+                  </div>
+                  <div className="">
+                    <div className="flex items-center  ">
+                      <div className="w-[70px] h-[70px] rounded-full bg-white overflow-hidden img-roud roundimg ">
+                        <img src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg" />
+                      </div>
+                      <div className="pl-5 parafont font-semibold">
+                        <p>{data.name}</p>
+                        <p className="pt-1">{data.sub}</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-
-          <div className="w-[30%]">
-            <div className="h-[30vh] flex  z-50 justify-center pl-2 ">
-              <motion.div
-                whileHover={{ scale: 1.1, transition: { duration: 0.5 } }}
-                exit={{ scale: 0, transition: { duration: 2 } }}
-                className="w-[100%] "
-              >
-                <div className="w-[90%] h-[25vh]  box flex justify-center items-center text-mes   ">
-                  <p className="w-[90%] text-justify z-20">
-                    printing and typesetting industry. Lorem ipsum some that
-                    industr's standard dummy text ever since the and them all
-                    that 1500s.
-                  </p>
-                </div>
-                <div className="triangle-down z-10"></div>
-              </motion.div>
-            </div>
-            <div className="">
-              <div className="   flex items-center  ">
-                <div className="w-[70px] h-[70px] rounded-full bg-white overflow-hidden img-roud roundimg ">
-                  <img src="https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg" />
-                </div>
-                <div className="pl-5">
-                  <p>Raju</p>
-                  <p className="pt-1">Subject who they are</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+            );
+          })}
+       
       </div>
     </div>
   );
---
+++ src/components/landing-section/Review/Reviewdata.js
@@ -0,0 +1,30 @@
+export const reviewdata = [
+  {
+    id: 1,
+    name: "Raju",
+    sub: "subject who they are",
+    mes: "more that all the more static and all the some kinds to bemore that all the more static and all the some kinds to be more that all the more static and all the some kinds to be that can all may could more that say s them",
+    img: "",
+  },
+  {
+    id: 2,
+    name: "Raju",
+    sub: "subject who they are",
+    mes: "more that all the more static and all the some kinds to bemore that all the more static and all the some kinds to be more that all the more static and all the some kinds to be that can all may could more that say s them",
+    img: "",
+  },
+  {
+    id: 3,
+    name: "Raju",
+    sub: "subject who they are",
+    mes: "more that all the more static and all the some kinds to bemore that all the more static and all the some kinds to be more that all the more static and all the some kinds to be that can all may could more that say s them",
+    img: "",
+  },
+  {
+    id: 4,
+    name: "Raju",
+    sub: "subject who they are",
+    mes: "more that all the more static and all the some kinds to bemore that all the more static and all the some kinds to be more that all the more static and all the some kinds to be that can all may could more that say s them",
+    img: "",
+  },
+];
